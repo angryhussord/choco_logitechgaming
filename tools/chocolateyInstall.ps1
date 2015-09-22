1734b9e23e40298cb4a2208882ecf024c8f21ea6
Patrick Hufford <angryhussord@outlook.com>
2015-09-22 13:48:45 -0700

    Updating for 8.74.80 driver release

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'logitechgaming'
 $installerType = 'EXE'
-$url = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.70.315_x86_Logitech.exe'
-$url64 = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.70.315_x64_Logitech.exe'
+$url = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.74.80_x86_Logitech.exe'
+$url64 = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.74.80_x64_Logitech.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
