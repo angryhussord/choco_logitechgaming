547b831ed3fb2503a75563eb39224efc7bc5c530
Patrick Hufford <pahuff@microsoft.com>
2016-02-28 10:22:51 -0800

    8.81.15 release update

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'logitechgaming'
 $installerType = 'EXE'
-$url = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.76.155_x86_Logitech.exe'
-$url64 = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.76.155_x64_Logitech.exe'
+$url = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.81.15_x86_Logitech.exe'
+$url64 = 'http://www.logitech.com/pub/techsupport/gaming/LGS_8.81.15_x64_Logitech.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
