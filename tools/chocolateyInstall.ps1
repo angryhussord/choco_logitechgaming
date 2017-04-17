7d294dc5b9ea892cd16300dbe08bf30b2fb602fa
Patrick Hufford <pahuff@microsoft.com>
2017-04-17 05:32:53 -0700

    fixing checksums

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,9 +15,9 @@ $packageArgs = @{
   silentArgs    = '/S'
   validExitCodes= @(0)
   softwareName  = 'logitechgaming*'
-  checksum      = 'F2893EA1AA57D115A433092055923EEF'
+  checksum      = 'CC23A3CC5EF0CF69A6B14C21BD9006A12E17CEECE66A1C919434AF7C4F26CE60'
   checksumType  = 'sha256'
-  checksum64      = '21976CBE9EF9BE0402B7AF5EAD69EA75'
+  checksum64      = 'B9F7B2F20D48E0E75AF1AD11AC80EDF124156569EED0B9A564D5B9B844272BD7'
   checksumType64  = 'sha256'
 };
 
