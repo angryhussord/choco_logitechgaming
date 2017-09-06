b0223fa08386375aff5ccf36a6a485f33b788c22
Patrick Hufford <pahuff@microsoft.com>
2017-09-06 06:15:09 -0700

    8.96.81 release

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'logitechgaming'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE'
-$url = 'https://download01.logi.com/web/ftp/pub/techsupport/gaming/LGS_8.94.108_x86_Logitech.exe'
-$url64 = 'https://download01.logi.com/web/ftp/pub/techsupport/gaming/LGS_8.94.108_x64_Logitech.exe'
+$url = 'https://download01.logi.com/web/ftp/pub/techsupport/gaming/LGS_8.96.81_x86_Logitech.exe'
+$url64 = 'https://download01.logi.com/web/ftp/pub/techsupport/gaming/LGS_8.96.81_x64_Logitech.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,9 +15,9 @@ $packageArgs = @{
   silentArgs    = '/S'
   validExitCodes= @(0)
   softwareName  = 'logitechgaming*'
-  checksum      = '4A745E7D9F625655152450E94D26B3E9F404C7D30BEBA537E86991FDEDCA605E'
+  checksum      = 'C6429C810213B11C2C580B46EA03B6E06CD17039CF4B28E5C1F882532BC2D7FA'
   checksumType  = 'sha256'
-  checksum64      = 'F9C20FC93FC9B26FCABCCDF7838613A447070345EE7F7E485E9B6EBC2E27046E'
+  checksum64      = '93B2BC80EFD9A130C73153C675BC62188C6059CC63023BB5D9B06B57B14DA03D'
   checksumType64  = 'sha256'
 };
 
